bd76c30e2ca218e9ac45e2b6c518c74ccd829a8b
Scapist1 <jaco6jaco6@gmail.com>
2025-09-18 22:41:02 +0200

    SparePartsInventoryAssistant

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#define partNumber 3
+bool askingAgain = 1;
 
-int main() {
-  int motors;
-  float kg_package;
+char buff[100];
+char parts[partNumber][15] = {"hydraulic pump", "PLC module", "servo motor"};
+char special[2][35] = {"Do you actually have any parts?",
+                       "Is there anything in stock at all?"};
 
-  // Input motors
-  printf("How many motors will carry the packages? ");
-  scanf("%d", &motors);
-  printf("Got it!\n");
+int main(void) {
+  printf("%s", "Aloha! - Welcome to the spare parts inventory\n\n");
 
-  // Input weight of the package in kg
-  printf("Determine how many kg of packages you got: ");
-  scanf("%f", &kg_package);
+  while (askingAgain == 1) {
+    printf("Which part do you need?\t>>\t"); // prompt a question
 
-  // Check if the motors can carry the package
-  if (motors * 5.6 < kg_package) {
-    printf(
-        "The %d motors won't carry the packages, %.2f kg is too heavy, sry \n",
-        motors, kg_package);
-  } else {
-    printf("Yes, the %d motors will carry the %.2f kg packages! :D\n", motors,
-           kg_package);
+    fgets(buff, sizeof(buff), stdin);
+    puts("");
+    int stringcount = strlen(buff);
+    buff[stringcount - 1] = '\0'; // cut of a
+
+    if ((strcmp(buff, "hydraulic pump") == 0) ||
+        (strcmp(buff, "PLC module") == 0) ||
+        (strcmp(buff, "servo motor") == 0)) {
+
+      printf("I've got %s here for you, bye!\n", buff);
+
+      askingAgain = 0;
+    } else if ((strcmp(buff, "Do you actually have any parts?") == 0) ||
+               (strcmp(buff, "Is there anything in stock at all?") == 0)) {
+      printf("We have %d part(s)!\n\n", partNumber);
+      for (int i = 0; i < partNumber; i++) {
+        printf("%s\n", parts[i]);
+      }
+    } else {
+      printf("I am afraid we don't have any %s in the inventory :(\n\n", buff);
+    }
   }
-
-  return 0;
 }
